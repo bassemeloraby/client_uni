2bd9680f588d0c32252955a8b52918c73fd7aefd
bassemeloraby <basem12683@gmail.com>
2025-12-01 00:19:43 +0300

    Update project title and integrate Tailwind CSS with additional dependencies. Remove unused CSS and refactor App component to use Tailwind for styling.

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+// @ts-nocheck
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["winter", "dracula"],
+  },
+};
+
+
