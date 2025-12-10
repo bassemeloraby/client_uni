950475e5715e838fcf165fc48791cb820755084e
bassemeloraby <basem12683@gmail.com>
2025-12-10 04:23:21 +0300

    Add Loading component to HomeLayout and update imports: integrate Loading component for page loading state management in HomeLayout, enhancing user experience during navigation.

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,15 +1,24 @@
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
-import { Header, Navbar } from "../components";
+import { Header, Navbar, Loading } from "../components";
+import { useNavigation } from "react-router-dom";
 
 const HomeLayout = () => {
+
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
+
   return (
     <Fragment>
       <Header />
       <Navbar />
-      <section className="align-element">
-        <Outlet />
-      </section>
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className="align-element">
+          <Outlet />
+        </section>
+      )}
     </Fragment>
 
   );
